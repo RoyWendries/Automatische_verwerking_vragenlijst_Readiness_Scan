c689983cbe00913c44f43406a22cdd4a4af46bd1
RoyWendries <1747290wendries@zuyd.nl>
2022-06-16 13:43:41 +0200

    manual input of dataset, with preprocessing and bidirectional lstm

--- LSTM_Test/LSTM_Test.py
+++ LSTM_Test/LSTM_Test.py
@@ -1,6 +1,6 @@
 
 # LSTM for sequence classification in the IMDB dataset
-import numpy
+import numpy as np
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
@@ -8,12 +8,18 @@ from keras.layers import LSTM
 from keras.layers import Embedding
 from keras.utils import pad_sequences
 # fix random seed for reproducibility
-numpy.random.seed(7)
+# numpy.random.seed(7)
 # load the dataset but only keep the top n words, zero the rest
 top_words = 5000
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
 # truncate and pad input sequences
-max_review_length = 500
+print(f'Number of rows & columns in X : {np.shape(X_train)}')
+print(f'Number of rows & columns in y : {np.shape(y_train)}')
+print(f'Number of categories in y : {len(np.unique(y_train))}')
+print(f'Categories in y : {np.unique(y_train)}')
+print(X_train)
+
+'''max_review_length = 500
 X_train = pad_sequences(X_train, maxlen=max_review_length)
 X_test = pad_sequences(X_test, maxlen=max_review_length)
 # create the model
@@ -29,4 +35,4 @@ print(model.summary())
 model.fit(X_train, y_train, epochs=3, batch_size=64)
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+print("Accuracy: %.2f%%" % (scores[1]*100))'''
